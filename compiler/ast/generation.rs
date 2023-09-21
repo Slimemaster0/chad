e62b6decf6d4ce7f7d8adde946f14fd116eaf3b0
Slimemaster <milo@munksgaard.eu>
2023-09-21 18:59:14 +0200

    Implement function for finding the function being edited

--- compiler/ast/generation.rs
+++ compiler/ast/generation.rs
@@ -5,11 +5,10 @@ use crate::format::*;
 use crate::ast::*;
 
 
-/// 
+/// generates an AST from a Vec<Token>
 pub fn generate(tokens: Vec<Token>, bin_type: BinType) -> AST {
     // {{{ Structs and enums
     struct Modifiers {
-        pub pub_: bool,
         pub mut_: bool,
         pub long: u8,
         pub short: bool,
@@ -19,7 +18,7 @@ pub fn generate(tokens: Vec<Token>, bin_type: BinType) -> AST {
 
     impl Modifiers {
         fn new() -> Modifiers {
-            Modifiers { pub_: false, mut_: false, long: 0, short: false, signed: false, unsigned: false}
+            Modifiers { mut_: false, long: 0, short: false, signed: false, unsigned: false}
         }
     }
     // }}}
@@ -32,17 +31,34 @@ pub fn generate(tokens: Vec<Token>, bin_type: BinType) -> AST {
         current_stmt: &mut usize,
         modifiers: &mut Modifiers,
         function_ception: &mut Vec<usize>,
-        name_space: &mut NameSpace,
+        name_space: &mut AST,
+        current_fn: &usize
         ) {
 
-        fn fn_finder(name_space: &NameSpace, ception: &Vec<usize>) {
-            
+        /// A function for finding the function being appended to.
+        /// The &Vec<usize> named i cannot be empty.
+        fn fn_finder<'a>(functions: &'a mut Vec<Function>, i: &Vec<usize>) -> &'a mut Function { // {{{
+            if i.len() == 0 {
+                panic!("{RED}Err:{RESET_FORMAT} fn_finder i should not be empty, plz report this is a bug in the compiler. UwU")
+            }
+            if i.len() == 1 {
+                return &mut functions[i[0]];
+            }
+            let mut j = i.clone();
+            let mut ret = fn_finder(&mut functions[i[0]].sub_functions, &j);
+            ret
+        } // }}}
+
+        fn name_extraction(input: &Token) -> String {
+            match input {
+                Token::Name(s) => return *s,
+                _ => panic!("Expected a name")
+            }
         }
 
         match tokens[*i] {
             Token::Modifier(m) => {
                 match m {
-                    Modifier::Pub => modifiers.pub_ = true,
                     Modifier::Mut => modifiers.mut_ = true,
                     Modifier::Long => modifiers.long += 1,
                     Modifier::Short => modifiers.short = true,
@@ -51,25 +67,21 @@ pub fn generate(tokens: Vec<Token>, bin_type: BinType) -> AST {
                 }
             }
 
-            Token::VarDecl(t) => {
-                let mut j: usize = *i + 1;
-                let mut name = String::new();
-                loop {
-                    match tokens[j] {
-                        Token::Name(s) => {
-                            name = s;
-                        }
-
-                        Token::Assign => {
-
-                        }
-
-                        Token::Semicolon => {
-                        }
-                        _ => panic!("{RED}Syntax-error:{RESET_FORMAT} Please specify the name of the variable"),
+            Token::VarDecl(mut t) => {
+                match t {
+                    Type::Struct(_) => {
+                        t = Type::Struct(name_extraction(&tokens[*i+1]));
+                        *i+=1;
                     }
-                    j+=1;
+
+                    Type::Enum(_) => {
+                        panic!("NOT implemented!");
+                    }
+
+                    _ => {}
                 }
+                let name: String = name_extraction(&tokens[*i+1]);
+                
                 *modifiers = Modifiers::new();
             }
 
@@ -106,45 +118,42 @@ pub fn generate(tokens: Vec<Token>, bin_type: BinType) -> AST {
 
     } // }}}
 
-    // {{{ The actual function
-    let mut ast = AST {
-        bin_type: bin_type,
-        name_spaces: vec![
-            NameSpace {
-                name: String::from("main"),
-                functions: vec![
-                    Function {
-                        name: String::from("main"),
-                        sub_functions: Vec::new(),
-                        is_public: false,
-                        arguments: Vec::new(),
-                        stmt: Vec::new(),
-                        VarDecls: Vec::new(),
-                    }
-                ]}]
-    };
-
-    { // Parse tokens
-        let mut current_ns: usize = 0;
-        let mut function_ception: Vec<usize> = Vec::new();
-        let mut context = String::new();
-        let mut current_stmt: usize = 0;
-        let mut modifiers = Modifiers::new();
-        for mut i in 0..tokens.len() {
-            parse(
-                &tokens,
-                &mut i,
-                &mut context,
-                &mut current_stmt,
-                &mut modifiers,
-                &mut function_ception,
-                &mut ast.name_spaces[current_ns]
-            )
+// {{{ The actual function
+let mut ast = AST {
+    bin_type: bin_type,
+    functions: vec![
+        Function {
+            name: String::from("main"),
+            sub_functions: Vec::new(),
+            arguments: Vec::new(),
+            stmt: Vec::new(),
+            VarDecls: Vec::new(),
         }
-    }
+    ]
+};
 
-    return ast;
-    // }}}
+{ // Parse tokens
+    let mut current_fn: usize = 0;
+    let mut function_ception: Vec<usize> = Vec::new();
+    let mut context = String::new();
+    let mut current_stmt: usize = 0;
+    let mut modifiers = Modifiers::new();
+    for mut i in 0..tokens.len() {
+        parse(
+            &tokens,
+            &mut i,
+            &mut context,
+            &mut current_stmt,
+            &mut modifiers,
+            &mut function_ception,
+            &mut ast,
+            &current_fn
+        )
+    }
+}
+
+return ast;
+// }}}
 }
 
 fn add_stmt(ast: &mut AST, function_ception: &Vec<usize>, input: Statement) {
